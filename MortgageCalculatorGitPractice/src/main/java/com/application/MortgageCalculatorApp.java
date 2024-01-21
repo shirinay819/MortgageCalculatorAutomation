aa763bce5e7bb976668460ad2b23af550a3713d8
shiri <shiri@shirinay.fios-router.home>
2024-01-20 19:10:13 -0700

    adding the project for the first time

---
+++ MortgageCalculatorGitPractice/src/main/java/com/application/MortgageCalculatorApp.java
@@ -0,0 +1,14 @@
+package com.application;
+
+/***
+ * @author Frank
+ */
+public class MortgageCalculatorApp {
+	
+	public static void main(String[] args) {
+		System.out.println("Mortgage Calculator application is");
+		
+		
+	}
+
+}
